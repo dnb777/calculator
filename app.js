ef0255e26b9263640ddff3036ba229e7d27f965f
Damian Bruno <damianbruno17@gmail.com>
2022-05-09 22:14:00 -0300

    Add functions for basic math operations

---
+++ app.js
@@ -0,0 +1,18 @@
+"use strict"
+
+
+function sum(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
