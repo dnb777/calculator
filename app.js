700dff9e85dcf25126b1f3f57e53d71621d0765a
Damian Bruno <damianbruno17@gmail.com>
2022-05-10 00:27:47 -0300

    Added obj for the operations and operate func to execute an operation based on user input

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
 "use strict"
 
 
-function sum(a, b) {
-	return a + b;
+// function sum(a, b) {
+// 	return a + b;
+// }
+
+// function subtract(a, b) {
+// 	return a - b;
+// }
+
+// function multiply(a, b) {
+// 	return a * b;
+// }
+
+// function divide(a, b) {
+// 	return a / b;
+// }
+const operations = {
+	"sum" : function(a, b) {
+		return a + b;
+	},
+	"subtract" : function(a, b) {
+		return a - b;
+	},
+	"multiply" : function(a, b) {
+		return a * b;
+	},
+	"divide" : function(a, b) {
+		return a / b;
+	},
 }
 
-function subtract(a, b) {
-	return a - b;
+
+function operate(n, n1) {
+	const op = prompt("enter operator", '')
+	if(!op){
+		console.log("ERROR: missing operator")
+	}
+
+	if(op == '+') return sum(n, n1);
+	if(op == '-') return subtract(n, n1);
+	if(op == '*') return multiply(n, n1);
+	if(op == '/') return divide(n, n1);
 }
 
-function multiply(a, b) {
-	return a * b;
-}
-
-function divide(a, b) {
-	return a / b;
-}
