7430d82069243c44a2aab39e9d468ee0ba814add
Damian Bruno <damianbruno17@gmail.com>
2022-05-12 18:37:05 -0300

    Added clear and delete function

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const buttons = document.querySelectorAll('.btn');
 const display = document.querySelector('#display');
 const equal = document.querySelector('#equal');
 const del = document.querySelector('#del');
-const clear = document.querySelector('#clearBtn')
+const clearBtn = document.querySelector('#clearBtn')
 const numbers = document.querySelectorAll('#num');
 const operators = document.querySelectorAll('#operator');
 
 let value = '';
 let op = '';
+
 
 const operations = {
 	"sum" : function(a, b) {
@@ -44,13 +45,15 @@ function operate(str) {
 	if(op == '%') return operations.percentage(a, b);
 }
 
-
+// When numbers are clicked it shows them on the display and
+//adds the number to "value"
 numbers.forEach(number => {
 	number.addEventListener('click', (e) => {
 		display.textContent += e.target.innerText;
 		value += e.target.innerText;
 	})
 })
+//Add the selected operator to "op" 
 operators.forEach(operator => {
 	operator.addEventListener('click', (e) => {
 		op = e.target.innerText;
@@ -58,7 +61,19 @@ operators.forEach(operator => {
 		value += e.target.innerText;
 	})
 })
-
+//Call the operate func with a string of numbers
 equal.addEventListener('click', () => {
 	display.textContent = operate(value);
+})
+
+//remove value from display and empty the value var
+clearBtn.addEventListener('click', () => {
+	value = '';
+	display.textContent = ''
+})
+
+//deletes last character
+del.addEventListener('click', () => {
+	value = value.slice(0, -1);
+	display.textContent = value;
 })
