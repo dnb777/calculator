6e854549e68d405ff031ffc8366b66e1824c7736
Damian Bruno <damianbruno17@gmail.com>
2022-05-17 20:49:56 -0300

    Edit app.js

--- app.js
+++ app.js
@@ -1,79 +1,82 @@
 "use strict"
 
-const buttons = document.querySelectorAll('.btn');
-const display = document.querySelector('#display');
-const equal = document.querySelector('#equal');
-const del = document.querySelector('#del');
-const clearBtn = document.querySelector('#clearBtn')
-const numbers = document.querySelectorAll('#num');
-const operators = document.querySelectorAll('#operator');
-
-let value = '';
-let op = '';
+let previousNumber = '';
+let currentNumber = '';
+let operation = '';
 
 
-const operations = {
-	"sum" : function(a, b) {
-		return a + b;
-	},
-	"subtract" : function(a, b) {
-		return a - b;
-	},
-	"multiply" : function(a, b) {
-		return a * b;
-	},
-	"divide" : function(a, b) {
-		return Number(Math.round(parseFloat((a / b) + 'e' + 2)) + 'e-' + 2);
-	},
-	"percentage" : function(a, b) {
-		return (a * b) / 100;
-	},
+const display = document.querySelector("#display");
+const numbersBtn = document.querySelectorAll("[data-number]");
+const operatorsBtn = document.querySelectorAll("[data-operator]");
+const equal = document.querySelector("#equal");
+const del = document.querySelector("#del");
+const clear = document.querySelector("#clear");
+
+
+function sum(a, b) {
+	return a + b;
+}
+
+function rest(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return Number(Math.round(parseFloat((a / b) + 'e' + 2)) + 'e-' + 2);
+}
+
+function percentage(a, b) {
+	return (a * b) / 100;
 }
 
 
-function operate(str) {
-	const nums = str.split(op);
+function operate(op, a, b) {
 
-	const a = Number(nums[0]);
-	const b = Number(nums[1]);
-
-
-	if(op == '+') return operations.sum(a, b);
-	if(op == '-') return operations.subtract(a, b);
-	if(op == 'x') return operations.multiply(a, b);
-	if(op == '/') return operations.divide(a, b);
-	if(op == '%') return operations.percentage(a, b);
+	if(op === "+") return sum(a,b);
+	if(op === "-") return rest(a,b);
+	if(op === "*") return multiply(a,b);
+	if(op === "/") return divide(a,b);
+	if(op === "%") return percentage(a,b);
 }
 
-// When numbers are clicked it shows them on the display and
-//adds the number to "value"
-numbers.forEach(number => {
-	number.addEventListener('click', (e) => {
-		display.textContent += e.target.innerText;
-		value += e.target.innerText;
+function updateDisplay() {
+	display.textContent = currentNumber;
+}
+
+function addNumber(number) {
+	if(number === '.' && currentNumber.includes('.')) return;
+	currentNumber += number;
+}
+
+function chooseOperation(op) {
+	if(currentNumber === '') return;
+	if(previousNumber !== '') {
+		compute();
+	}
+
+	operation = op;
+	previousNumber = currentNumber;
+	currentNumber = '';
+}
+
+
+
+
+
+numbersBtn.forEach(button => {
+	button.addEventListener('click', () => {
+		addNumber(button.textContent);
+		updateDisplay();
 	})
 })
-//Add the selected operator to "op" 
-operators.forEach(operator => {
-	operator.addEventListener('click', (e) => {
-		op = e.target.innerText;
-		display.textContent += op;
-		value += e.target.innerText;
+
+operatorsBtn.forEach(button => {
+	button.addEventListener('click', () => {
+		chooseOperation(button.textContent);
+		updateDisplay();
 	})
-})
-//Call the operate func with a string of numbers
-equal.addEventListener('click', () => {
-	display.textContent = operate(value);
-})
-
-//remove value from display and empty the value var
-clearBtn.addEventListener('click', () => {
-	value = '';
-	display.textContent = ''
-})
-
-//deletes last character
-del.addEventListener('click', () => {
-	value = value.slice(0, -1);
-	display.textContent = value;
 })
