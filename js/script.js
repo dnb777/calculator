c1628af568ed72585e8ee61f1fb84bed8bf4049f
Damian Bruno <damianbruno17@gmail.com>
2024-12-10 17:07:49 -0300

    feat: crea variables currentNumber, previousNumber y operator

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const display = document.querySelector(".display");
 
+let currentNumber = "";
+let previousNumber = "";
+let operator = "";
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
