fd191f895eea546b205841e6cab2f8cb3eb99d47
Damian Bruno <damianbruno17@gmail.com>
2024-12-10 14:19:37 -0300

    feat: agrega función operate() - la función toma toma dos números y un operador - en base al operador pasado llama a la función correspondiente

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+
+function operate(n1, n2, op){
+    console.log(op);
+    switch(op){
+        case "+":
+            return add(n1, n2);
+            break;
+        case "-":
+            return subtract(n1, n2);
+            break;
+        case "x":
+            return multiply(n1, n2);
+            break;
+        case "%":
+            return divide(n1, n2);
+            break;
+    }
+}
