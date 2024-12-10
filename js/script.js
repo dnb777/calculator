d85ecb5e4b3793c1d69721b89fde85e1b89b7e2e
Damian Bruno <damianbruno17@gmail.com>
2024-12-10 14:59:31 -0300

    feat: agrega refreshDisplay() - actualiza el texto del display

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const display = document.querySelector(".display");
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
@@ -21,4 +22,8 @@ function operate(n1, n2, op){
             return divide(n1, n2);
             break;
     }
+}
+
+function refreshDisplay(text){
+    display.textContent = text;
 }
