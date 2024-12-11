a54d0c1747a73eef28311c2b478b1913b964460d
Damian Bruno <damianbruno17@gmail.com>
2024-12-11 13:59:36 -0300

    feat: agrega evento para los botones y variables para los resultados - al disparase el evento se filtra el contenido con los valores de numbers y operators - si el valor es valido se almacena temporalmente en una de las variables - se llama a refreshDisplay para actualizar el resultado en pantalla - cuando el valor pasado es '=' se llama a operate con los valores actuales - currentValue retiene el resultado actual

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
 const display = document.querySelector(".display");
+const buttons = document.querySelectorAll(".btn");
+const numbers = ['0','2','1','2','3','4','5','6','7','8','9','.'];
+const operators = ['+','-','x','%'];
 
-let currentNumber = "";
-let previousNumber = "";
+let currentValue = "";
+let previousValue = "";
 let operator = "";
+let result = "";
+
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        const value = e.target.textContent;
+        
+        if (!currentValue && numbers.includes(value)){
+            currentValue = parseFloat(value);
+            refreshDisplay(previousValue + operator + currentValue );
+        }else if (operators.includes(value)){
+            previousValue = currentValue;
+            currentValue = "";
+            operator = value;
+            refreshDisplay(previousValue + operator + currentValue)
+        }else if (value == "="){
+            result = operate(previousValue, currentValue, operator);
+            refreshDisplay(result);
+            currentValue = result;
+        }
+    })
+})
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
@@ -11,7 +35,6 @@ const divide = (a, b) => a / b;
 
 
 function operate(n1, n2, op){
-    console.log(op);
     switch(op){
         case "+":
             return add(n1, n2);
