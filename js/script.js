1108059a9ccd1a681433cbfcc0c39c9c0f28c216
Damian Bruno <damianbruno17@gmail.com>
2024-12-11 17:05:35 -0300

    refactor: modifica las variables a arrays - se cambio para poder manejar correctamente numeros mayores a dos cifras - tambien maneja correctamente los numeros con punto decimal

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ const buttons = document.querySelectorAll(".btn");
 const numbers = ['0','2','1','2','3','4','5','6','7','8','9','.'];
 const operators = ['+','-','x','%'];
 
-let currentValue = "";
-let previousValue = "";
+let currentValue = [];
+let previousValue = [];
 let operator = "";
 let result = "";
 
@@ -12,18 +12,18 @@ buttons.forEach(button => {
     button.addEventListener("click", (e) => {
         const value = e.target.textContent;
         
-        if (!currentValue && numbers.includes(value)){
-            currentValue = parseFloat(value);
-            refreshDisplay(previousValue + operator + currentValue );
+        if (numbers.includes(value)){
+            currentValue.push(value);
+            display.textContent = previousValue.join("") + operator + currentValue.join("");
         }else if (operators.includes(value)){
-            previousValue = currentValue;
-            currentValue = "";
+            previousValue = Array.from(currentValue);
+            currentValue.splice(0);
             operator = value;
-            refreshDisplay(previousValue + operator + currentValue)
+            display.textContent = previousValue.join("") + operator + currentValue.join("");
         }else if (value == "="){
             result = operate(previousValue, currentValue, operator);
             refreshDisplay(result);
-            currentValue = result;
+            currentValue.splice(0, currentValue.length, result);
         }
     })
 })
@@ -35,18 +35,21 @@ const divide = (a, b) => a / b;
 
 
 function operate(n1, n2, op){
+    let num1 = parseFloat(n1.join(""));
+    let num2 = parseFloat(n2.join(""));
+    
     switch(op){
         case "+":
-            return add(n1, n2);
+            return add(num1, num2);
             break;
         case "-":
-            return subtract(n1, n2);
+            return subtract(num1, num2);
             break;
         case "x":
-            return multiply(n1, n2);
+            return multiply(num1, num2);
             break;
         case "%":
-            return divide(n1, n2);
+            return divide(num1, num2);
             break;
     }
 }
