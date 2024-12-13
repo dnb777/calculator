c2fb84cdfc732623b546bad2ce375af84c7594fe
Damian Bruno <damianbruno17@gmail.com>
2024-12-13 20:05:05 -0300

    feat: agrega nuevas funcionalidades

--- js/script.js
+++ js/script.js
@@ -1,79 +1,97 @@
+const calculator = {
+    operands: ['0','2','1','2','3','4','5','6','7','8','9','.'],
+    operators: ['+','-','*','/'],
+    operations: {
+        add(a, b) {return a + b},
+        subtract(a, b) {return a - b},
+        multiply(a, b) {return a * b},
+        divide(a, b) {return a / b},
+    },
+    clearAll() {
+        display.textContent = "";
+        currentValue = "";
+        previousValue = "";
+        operator = "";
+        result = "";
+    },
+    operate(value1, value2, op) {
+        if (!value2 || !op) return value1;
+
+        let num1 = parseFloat(value1);
+        let num2 = parseFloat(value2);
+        let result;
+
+        switch(op){
+            case "+":
+                result = this.operations.add(num1, num2);
+                return  result % 1 !== 0 ? result.toFixed(2) : `${result}`;
+            case "-":
+                result = this.operations.subtract(num1, num2);
+                return  result % 1 !== 0 ? result.toFixed(2) : `${result}`;
+            case "*":
+                result = this.operations.multiply(num1, num2);
+                return  result % 1 !== 0 ? result.toFixed(2) : `${result}`;
+            case "/":
+                result = this.operations.divide(num1, num2);
+                return  result % 1 !== 0 ? result.toFixed(2) : `${result}`;
+        }
+    },
+    populateDisplay(text) {
+        return display.textContent = text;
+    },
+}
+
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll(".btn");
 const clear = document.querySelector("#clear");
-const numbers = ['0','2','1','2','3','4','5','6','7','8','9','.'];
-const operators = ['+','-','*','/'];
+const backspace = document.querySelector("#backspace");
 
-let currentValue = [];
-let previousValue = [];
+let currentValue = "";
+let previousValue = "";
 let operator = "";
 let result = "";
 
+
 buttons.forEach(button => {
-    button.addEventListener("click", (e) => {
+    button.addEventListener('click', (e) => {
         const value = e.target.textContent;
         calculate(value);
     })
 })
 
-document.addEventListener("keypress", (e) => {
-    console.log(e.code)
-    const valueKey = e.key;
-    calculate(valueKey);
+document.addEventListener("keydown", (e) => {
+    const value = e.key;
+    calculate(value);
 })
 
 function calculate(value){
-    if (value == "C") clearAll();
-        
-    if (numbers.includes(value)){
-        currentValue.push(value);
-        display.textContent = previousValue.join("") + operator + currentValue.join("");
-    }else if (operators.includes(value)){
-        previousValue = Array.from(currentValue);
-        currentValue.splice(0);
+    if (value == "AC" || value == "Delete") calculator.clearAll();
+    if (value == "del" || value == "Backspace") currentValue = currentValue.slice(0, -1);
+    
+    if (value == "=" || value == "Enter"){
+        if (operator == "/" && currentValue == "0"){
+            calculator.clearAll();
+            return calculator.populateDisplay("404: Math not found");
+        }
+        result = calculator.operate(previousValue, currentValue, operator);
+        currentValue = result;
+        previousValue = "";
+        operator = "";
+        calculator.populateDisplay(result);
+    }else if(
+        operator != "" && 
+        previousValue != "" && 
+        calculator.operators.includes(value)) {
+            result = calculator.operate(previousValue, currentValue, operator);
+            currentValue = result;
+            previousValue = "";
+    }else if (calculator.operands.includes(value)){
+        if (value == "." && currentValue.includes(value)) return;
+        currentValue += value;
+    }else if (calculator.operators.includes(value)){
         operator = value;
-        display.textContent = previousValue.join("") + operator + currentValue.join("");
-    }else if (value == "=" || value == "Enter"){
-        result = operate(previousValue, currentValue, operator);
-        refreshDisplay(result);
-        currentValue.splice(0, currentValue.length, result);
+        previousValue = currentValue;
+        currentValue = "";
     }
-}
-
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
-
-
-function operate(n1, n2, op){
-    let num1 = parseFloat(n1.join(""));
-    let num2 = parseFloat(n2.join(""));
-
-    switch(op){
-        case "+":
-            return add(num1, num2);
-            break;
-        case "-":
-            return subtract(num1, num2);
-            break;
-        case "*":
-            return multiply(num1, num2);
-            break;
-        case "/":
-            return divide(num1, num2);
-            break;
-    }
-}
-
-function refreshDisplay(text){
-    display.textContent = text;
-}
-
-function clearAll(){
-    currentValue.splice(0);
-    previousValue.splice(0);
-    operator = "";
-    result = "";
-    refreshDisplay();
+    calculator.populateDisplay(`${previousValue}${operator}${currentValue}`);
 }
